92dd0d13f9505c804a2fda4d9d7d0fe5db420e4f
Blair Watkinson <blair.watkinson@me.com>
2021-03-20 02:53:15 +0000

    first commit

---
+++ jupyter-conf.py
@@ -0,0 +1,7 @@
+c.NotebookApp.base_url       = "/proxy/8696347b-763d-4aa4-bc6e-cc5962005416/"
+c.NotebookApp.allow_origin   = "*"
+c.NotebookApp.trust_xheaders = True
+c.NotebookApp.open_browser   = False
+c.NotebookApp.allow_root     = True
+c.NotebookApp.ip             = "0.0.0.0"
+c.NotebookApp.token          = ""
